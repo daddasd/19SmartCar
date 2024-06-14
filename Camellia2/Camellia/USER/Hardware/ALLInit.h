8e9bd163b0fd2e26116d2054ad828c02be10aa23
Rush <3323169544@qq.com>
2024-06-14 18:50:10 +0800

    test cmt

--- Camellia2/Camellia/USER/Hardware/ALLInit.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ALLINIT_H_
-#define _ALLINIT_H_
-
-void GPIO_Init(void);
-void ALL_Peripheral_Init(void);
-
-
-#endif
--- Camellia2/Camellia/USER/Hardware/vofa.h
+++
@@ -1,6 +0,0 @@
-#ifndef _VOFA_H
-#define _VOFA_H
-
- void Send4DataToVofa(float _a,float _b, float _c, float _d,float _e,float _f);
-
-#endif
